234d13bc9dccf7bb0484ad389b7d98bc69d2fa31
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-03-30 14:36:57 +0200

    feat(contacts): Reading user-contacts and display them in the RecycleView

--- app/src/main/java/com/martijn/zentjens/mbdand_assessment/PhoneBookActivity.java
+++ app/src/main/java/com/martijn/zentjens/mbdand_assessment/PhoneBookActivity.java
@@ -1,16 +1,26 @@
 package com.martijn.zentjens.mbdand_assessment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.Toast;
 
 import com.martijn.zentjens.mbdand_assessment.models.Contact;
 
 import java.util.ArrayList;
 
 public class PhoneBookActivity extends AppCompatActivity {
+    private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 1;
     ArrayList<Contact> contactList;
 
     @Override
@@ -18,18 +28,77 @@ public class PhoneBookActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_book);
 
+        // Get RecycleView from the layout
         RecyclerView phoneBookRecyclerView = (RecyclerView) findViewById(R.id.phone_book_list);
 
-        // Initialize contacts
-        contactList = new ArrayList<>();
-        contactList.add(new Contact("Martijn Zentjens"));
-        contactList.add(new Contact("Jesse Stupers"));
+        // Get permission
+        this.getPermissionToReadContacts();
+        // Get all user-contacts
+        this.getUserContacts();
 
-        // Create adapter passing in the sample user data
-        PhoneBookRecycleViewAdapter adapter = new PhoneBookRecycleViewAdapter(contactList);
         // Attach the adapter to the recyclerview to populate items
+        PhoneBookRecycleViewAdapter adapter = new PhoneBookRecycleViewAdapter(contactList);
         phoneBookRecyclerView.setAdapter(adapter);
         // Set layout manager to position the items
         phoneBookRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    // Get permission of the user to read contacts
+    public void getPermissionToReadContacts() {
+        if (!this.checkHasReadContactsPermission() &&
+                !ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, READ_CONTACTS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    // When permission has been given for the first time
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == READ_CONTACTS_PERMISSIONS_REQUEST) {
+            boolean hasPermission = grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            this.createToast("Het uitlezen van contacten is " + (hasPermission ? "toegestaan" : "afgewezen"));
+
+            if (hasPermission) this.getUserContacts();
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    // Check if has permission to read contacts
+    public boolean checkHasReadContactsPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    // Display simple message to the user
+    public void createToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    // Read user contacts from phone
+    public void getUserContacts() {
+        contactList = new ArrayList<>();
+
+        // Check if there is permission
+        if (this.checkHasReadContactsPermission()) {
+            Uri contactUri = ContactsContract.Contacts.CONTENT_URI;
+
+            String[] PROJECTION = new String[]{
+                    ContactsContract.Contacts._ID,
+                    ContactsContract.Contacts.DISPLAY_NAME,
+                    ContactsContract.Contacts.HAS_PHONE_NUMBER,
+            };
+
+            // Example query on phonenumbers
+            String SELECTION = ContactsContract.Contacts.HAS_PHONE_NUMBER + "='1'";
+            Cursor contacts = managedQuery(contactUri, PROJECTION, SELECTION, null, null);
+
+            try {
+                while (contacts.moveToNext()) {
+                    contactList.add(new Contact(contacts.getString(1)));
+                }
+            } finally {
+                contacts.close();
+            }
+        }
     }
 }
