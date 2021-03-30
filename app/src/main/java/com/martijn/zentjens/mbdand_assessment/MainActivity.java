bb0f0a0e5b83cc33ecbc153e052e2c44ccdd8c1c
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-03-30 10:52:32 +0200

    feat(navigation): Create button for navigation to Table

--- app/src/main/java/com/martijn/zentjens/mbdand_assessment/MainActivity.java
+++ app/src/main/java/com/martijn/zentjens/mbdand_assessment/MainActivity.java
@@ -1,8 +1,12 @@
 package com.martijn.zentjens.mbdand_assessment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Example button load new Activity
+    public void goToAnActivity(View view) {
+        /*Fragment anotherFragment = Fragment.instantiate(this, PhoneBookRecyclerViewAdapter.class.getName());
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        //ft.add(R.id.realtabcontent, anotherFragment);
+        ft.addToBackStack(null);
+        ft.commit();*/
     }
 }
